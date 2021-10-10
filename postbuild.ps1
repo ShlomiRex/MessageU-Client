1364d33f1958b7fd521ebe3aa3a039372b94b60a
Shlomi <vgtvgy1@gmail.com>
2021-10-11 00:16:05 +0300

    added proper readme

--- postbuild.ps1
+++ postbuild.ps1
@@ -1,2 +1,3 @@
 copy /Y "$(SolutionDir)Debug\$(SolutionName).exe" "$(SolutionDir)Debug\Client_Neo\"
 copy /Y "$(SolutionDir)Debug\$(SolutionName).exe" "$(SolutionDir)Debug\Client_ShlomiRex\"
+copy /Y "$(SolutionDir)Debug\$(SolutionName).exe" "$(SolutionDir)Debug\Client_Unregistered\"
